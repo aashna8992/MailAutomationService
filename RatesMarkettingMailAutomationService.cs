97c21e53356e9a050b605fc8b2f38b7f48ec32b4
Aashna.Butala <Aashna.Butala@fremontbank.com>
2019-10-11 13:17:20 -0700

    adding files

---
+++ RatesMarkettingMailAutomationService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace MailAutomationService
+{
+    public partial class RatesMarkettingMailAutomationService : ServiceBase
+    {
+        private static Timer timer;
+        public RatesMarkettingMailAutomationService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Service Started");
+            timer = new Timer(120000);
+            timer.Enabled = true;
+            timer.Elapsed += OnElapsedTime;
+            timer.AutoReset= true;
+        }
+
+        private static void OnElapsedTime(Object source, ElapsedEventArgs e)
+        {
+            WriteToFile("Service is recall at " + DateTime.Now);
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service Ended");
+        }
+
+        public static void WriteToFile(string Message)
+        {
+            try
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+
+                if (!File.Exists(filepath))
+                {
+                    // Create a file to write to.   
+                    using (StreamWriter sw = File.CreateText(filepath))
+                    {
+                        sw.WriteLine(Message);
+                    }
+                }
+                else
+                {
+                    using (StreamWriter sw = File.AppendText(filepath))
+                    {
+                        sw.WriteLine(Message);
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
